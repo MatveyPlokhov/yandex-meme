7efbe0ed2236ff314102287bdc59716d7ddfb9d3
mapl <matvey.pavlovich@yandex.ru>
2020-03-01 01:38:15 +0300

    c

--- main.py
+++ main.py
@@ -79,8 +79,8 @@ def handle_dialog(req, res):
         #res['response']['card']['button']['text'] = 'mainText'
         #res['response']['card']['title'] = mainText
         #res['response']['text'] = get_buttons(user_id)
-        res['response']['card']['type'] = 'BigImage'
-        res['response']['card']['image_id'] = r.json()['image']['id']
+        res['response']['card']['type'] = BigImage
+        #res['response']['card']['image_id'] = r.json()['image']['id']
         return
 
     res['response']['buttons'] = get_buttons(user_id)
